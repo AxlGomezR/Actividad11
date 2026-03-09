dbe5a2e3ab623cef043c1ceb95061bbaa393ac85
Axl Gómez <axljavigomez@gmail.com>
2026-03-08 20:49:11 -0600

    Creación de la función Factorial con parámetro n

--- Func24/Func24/Program.cs
+++ Func24/Func24/Program.cs
@@ -8,6 +8,21 @@ namespace Func24
 {
     internal class Program
     {
+        static int Factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }else
+            {
+                int fact = 1;
+                for (int i = 1; i <= n; i++)
+                {
+                    fact *= i;
+                }
+                return fact;
+            }
+        }
         static void Main(string[] args)
         {
         }
