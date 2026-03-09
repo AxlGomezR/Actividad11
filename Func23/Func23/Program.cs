9a6b60a199eff365d8fa06f7d51979ba1b299d39
Axl Gómez <axljavigomez@gmail.com>
2026-03-08 20:43:56 -0600

    Solicitar al usuario la cantidad de números que desea ingresar y verificar que sea al menos uno

--- Func23/Func23/Program.cs
+++ Func23/Func23/Program.cs
@@ -8,7 +8,7 @@ namespace Func23
 {
     internal class Program
     {
-        static int SumarDel1AlN(int n)
+        static void SumarDel1AlN(int n)
         {
             int suma = 0;
             for (int i = 1; i <= n; i++)
@@ -17,11 +17,27 @@ namespace Func23
                 int numero= int.Parse( Console.ReadLine() );
                 suma += numero;
             }
-            return suma;
+            Console.WriteLine($"La suma de los números ingresados es {suma}");
         }
         static void Main(string[] args)
         {
-            
+            int n;
+            bool continuar = false;
+            do
+            {
+                Console.WriteLine("Ingrese la cantidad de números que desea sumar");
+                n = int.Parse(Console.ReadLine());
+                Console.Clear();
+                if (n > 0)
+                {
+                    continuar = true;
+                }
+                else
+                {
+                    Console.WriteLine("Debe ingresar al menos un número\n");
+                }
+            } while (continuar == false);
+            SumarDel1AlN(n);
         }
     }
 }
