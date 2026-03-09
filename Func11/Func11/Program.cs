5b58e61dddc45c08b2710ad11bb352efddf212b8
Axl Gómez <axljavigomez@gmail.com>
2026-03-08 19:20:12 -0600

    Solicitar dos números al usuario y llamar a la función sumar

--- Func11/Func11/Program.cs
+++ Func11/Func11/Program.cs
@@ -8,13 +8,19 @@ namespace Func11
 {
     internal class Program
     {
-        int sumar(int a, int b)
+        static int Sumar(int a, int b)
         {
             return a + b;
         }
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Ingrese el primer número");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo número");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.Clear();
+            int suma = Sumar(num1, num2);
+            Console.WriteLine($"La suma de los números ingresados es = {suma}");
         }
     }
 }
