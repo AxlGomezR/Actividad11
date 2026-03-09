dc77f5457caeb4d1bb1d6f0d66d38dc4734323c6
Axl Gómez <axljavigomez@gmail.com>
2026-03-08 21:00:52 -0600

    Creación de la Función EsPrimo con parámetro n que retorna un valor booleano

--- Func25/Func25/Program.cs
+++ Func25/Func25/Program.cs
@@ -8,6 +8,19 @@ namespace Func25
 {
     internal class Program
     {
+        static bool EsPrimo(int n)
+        {
+            bool primo = true;
+            for(int i =2; i<n; i++)
+            {
+                if (n % i == 0)
+                {
+                    primo = false;
+                    break;
+                }
+            }
+            return primo;
+        }
         static void Main(string[] args)
         {
         }
