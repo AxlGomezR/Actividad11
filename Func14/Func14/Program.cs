70e73f0fa33aa9ec8a5d8a8148f67e8eb6421830
Axl Gómez <axljavigomez@gmail.com>
2026-03-08 19:30:22 -0600

    Creación de la función Dividir con parámetros a y b, que retornará la división de los parámetros

--- Func14/Func14/Program.cs
+++ Func14/Func14/Program.cs
@@ -8,6 +8,10 @@ namespace Func14
 {
     internal class Program
     {
+        static double Dividir(double a, double b)
+        {
+            return a / b;
+        }
         static void Main(string[] args)
         {
         }
