f0ed969c688dbcf87668e34430bd095b868715ac
Axl Gómez <axljavigomez@gmail.com>
2026-03-08 20:39:44 -0600

    Creación de la función SumarDel1AlN con parámetro n que solicita al usuario n números mientras los suma

--- Func23/Func23/Program.cs
+++ Func23/Func23/Program.cs
@@ -8,8 +8,20 @@ namespace Func23
 {
     internal class Program
     {
+        static int SumarDel1AlN(int n)
+        {
+            int suma = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Ingrese el número {i}");
+                int numero= int.Parse( Console.ReadLine() );
+                suma += numero;
+            }
+            return suma;
+        }
         static void Main(string[] args)
         {
+            
         }
     }
 }
