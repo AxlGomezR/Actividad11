c9d894ad2e245b8b391656adb0584cca804846d2
Axl Gómez <axljavigomez@gmail.com>
2026-03-08 20:02:38 -0600

    Creación de la función MayorDeTres con parámetros a, b y c que compara los tres números y retorna el valor mayor

--- Func18/Func18/Program.cs
+++ Func18/Func18/Program.cs
@@ -8,6 +8,19 @@ namespace Func18
 {
     internal class Program
     {
+        static int MayorDeTres(int a, int b, int c)
+        {
+            int mayor = a;
+            if (b > mayor)
+            {
+                mayor = b;
+            }
+            if (c > mayor)
+            {
+                mayor = c;
+            }
+            return mayor;
+        }
         static void Main(string[] args)
         {
         }
