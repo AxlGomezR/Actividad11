94b74768713385a9d845863677d2223b9e339b4a
Axl Gómez <axljavigomez@gmail.com>
2026-03-08 20:10:51 -0600

    Solicitar al usuario dos números y llamar a la función EsMultiplo. Verificar el valor que devuelve y mostrar mensaje

--- Func19/Func19/Program.cs
+++ Func19/Func19/Program.cs
@@ -8,8 +8,32 @@ namespace Func19
 {
     internal class Program
     {
+        static bool EsMultiplo(int n, int m)
+        {
+            if (n % m == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Ingrese un número");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el número que desea verificar");
+            int m = int.Parse(Console.ReadLine());
+            Console.Clear();
+            if (EsMultiplo(n, m))
+            {
+                Console.WriteLine($"El número {m} es múltiplo de {n}");
+            }
+            else
+            {
+                Console.WriteLine($"El número {m} no es múltiplo de {n}");
+            }
         }
     }
 }
